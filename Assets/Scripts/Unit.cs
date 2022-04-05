25b14c0012112ab9c53bcd7d8a6d5181ebd03dec
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2022-04-05 16:26:31 -0400

    units now lose aim on enemy when they go out of range, must aim at enemy again, if there was no other enemy to change targets to

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -44,7 +44,7 @@ public class Unit : MonoBehaviour
     // Denotes the position the unit is going to
     private Vector3 targetPosition;
     // Denotes if the unit is in an aiming state, basically, a NEW target has appeared and it is waiting on its aiming speed
-    private bool startedAim;
+    private bool startedAimingPhase;
     // Denotes if the unit is currently aimed at an enemy, so it no longer has to wait for its aiming speed
     private bool aimedAtEnemy;
     // Denotes if the unit can move, which it cannot if it just shot
@@ -88,7 +88,7 @@ public class Unit : MonoBehaviour
         startAimTime = 0;
         startShootTime = 0;
         aimedAtEnemy = false;
-        startedAim = false;
+        startedAimingPhase = false;
         canMove = true;
         prevClosestEnemy = null;
         targetPosition = this.transform.position;
@@ -151,7 +151,7 @@ public class Unit : MonoBehaviour
         }
 
         // Two variables to store the current closest distance and current closest enemy (not necessarily within range)
-        float closestDistance = float.MaxValue;
+        float closestEnemyDistance = float.MaxValue;
         GameObject closestEnemy = null;
 
         // Loop through all enemies
@@ -167,7 +167,7 @@ public class Unit : MonoBehaviour
                              (unit.transform.position.z - this.transform.position.z) * (unit.transform.position.z - this.transform.position.z));
 
             // closestDistance is the distance to the closest enemy
-            if (distance < closestDistance)
+            if (distance < closestEnemyDistance)
             {
                 // This chunk checks to see if the unit is trying to shoot through a physical barrier such as a house by using a raycast
                 RaycastHit hit;
@@ -176,7 +176,7 @@ public class Unit : MonoBehaviour
                     if (hit.transform == unit.transform)
                     {
                         // If the enemy is going to be the current closest enemy and is also in line-of-sight, we save it to be checked further
-                        closestDistance = distance;
+                        closestEnemyDistance = distance;
                         closestEnemy = unit;
 
                     }
@@ -186,20 +186,28 @@ public class Unit : MonoBehaviour
         }
 
         // Now we check if the closest enemy is in range or not
-        if (closestDistance < range)
+        if (closestEnemyDistance < range)
         {
-                  
+            // make sure that the enemy is still "alive" aka not set to null
             if (closestEnemy != null)
-            { 
+            {
+                // This if statement checks if the unit is locking onto a new enemy or not
+                // If the previous enemy that it was aiming at is different from the new closest enemy,
+                // will reset fields startedAim and aimedAtEnemy to start aiming process again.
+                // The purpose of this is to make sure the unit goes through the "aiming phase" before
+                // starting to attack a new target.
                 if (closestEnemy != prevClosestEnemy) {
-                    startedAim = false;
+                    startedAimingPhase = false;
                     aimedAtEnemy = false;
                     prevClosestEnemy = closestEnemy;
                 }
                 
-                if (!startedAim && !aimedAtEnemy) {
+                // If the unit is neither already aimed at an enemy or currently in
+                // the aimingPhase (startedAimingPhase = true), start the aiming phase
+                // by logging the current time and setting startedAimingPhase to true.
+                if (!startedAimingPhase && !aimedAtEnemy) {
                     startAimTime = Time.time;
-                    startedAim = true;
+                    startedAimingPhase = true;
                 }
 
                 // This draws a line from the unit's current position to the closest enemy in range.
@@ -210,7 +218,7 @@ public class Unit : MonoBehaviour
                 if (startAimTime + aimSpeed <= Time.time) 
                 {
                     aimedAtEnemy = true;
-                    startedAim = false;
+                    startedAimingPhase = false;
 
                     if (isCanAttack() && !ignoreEnemy)
                     {
@@ -222,13 +230,16 @@ public class Unit : MonoBehaviour
             }
             else
             {
-                startedAim = false;
+                startedAimingPhase = false;
                 aimedAtEnemy = false;
             }
         }
-        else
+        else // whatever the closest target is, is not in range of the unit
         {
-            // This makes it so no line is drawn since it is the same point.
+            // if target left the range, the previousClosestEnemy is disregarded, as unit will need to reaim
+            // if rengaging witht the same target.
+            prevClosestEnemy = null;
+            // This makes it so no line is drawn since it is the same point
             lineRenderer.SetPosition(0, new Vector3(0,0,0));
             lineRenderer.SetPosition(1, new Vector3(0,0,0));
         }
